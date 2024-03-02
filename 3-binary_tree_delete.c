776637a9bb3f5563a56f67e5e4d54bb8a6cd7e6d
MalekAjil <aboanasajil@gmail.com>
2024-03-02 12:45:28 -0800

    task0

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,27 +7,33 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
 	binary_tree_t *tmp = tree, *prnt = NULL;
+	int isLeft = 1;
 
 	if (tree == NULL)
 		return;
 	while (tmp != NULL)
 	{
-		printf("%d\n", tmp->n);
 		if (tmp->left != NULL)
 		{
-			prnt = tmp;
 			tmp = tmp->left;
+			isLeft = 1;
 		}
 		else if (tmp->right != NULL)
 		{
-			prnt = tmp;
 			tmp = tmp->right;
+			isLeft = 0;
 		}
 		else
 		{
+			binary_tree_print(tree);
 			prnt = tmp->parent;
 			free(tmp);
+			if (isLeft)
+				prnt->left = NULL;
+			else
+				prnt->right = NULL;
 			tmp = prnt;
+			binary_tree_print(tree);
 		}
 	}
 }
